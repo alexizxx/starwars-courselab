e63888aae65fc045cb3bc697c079b2a8059768e6
alexizxx <sedymov03@bk.ru>
2026-02-20 02:38:13 +0300

    javadoc added

---
+++ build/docs/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.example"},{"l":"org.example.dto"},{"l":"org.example.service"},{"l":"org.example.util"}];updateSearchResults();
